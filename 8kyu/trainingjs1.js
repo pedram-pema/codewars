160da474d59c303fb1f09c5e65fa5053ae1f7efd
pedram-pema <studentpedramp@gmail.com>
2023-11-06 18:55:30 +0330

    Write the first training function

--- 8kyu/trainingjs1.js
+++ 8kyu/trainingjs1.js
@@ -1,3 +1,7 @@
 /* Training JS #1: create your first JS function and print "Hello World!":
 https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8. */
 
+function helloWorld() {
+    let str = 'Hello World!';
+    console.log(str);
+}
