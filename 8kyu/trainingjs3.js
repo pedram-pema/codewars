c953f336fb882db4539b01be380d69616b4a44b2
pedram-pema <studentpedramp@gmail.com>
2023-11-13 09:45:12 +0330

    Complete the 3rd training

--- 8kyu/trainingjs3.js
+++ 8kyu/trainingjs3.js
@@ -1,7 +1,7 @@
 /* Training JS #3: Basic data types--String
 https://www.codewars.com/kata/571edea4b625edcb51000d8e */
 
-DESCRIPTION:
+/* DESCRIPTION:
 In javascript, String is one of basic data types. Use "" or '' to declare a string variable. It can be letters:a,b,c,A,B,C, numbers: 123..., Various symbol:@#$%^&* , Control symbol:\n,\t etc..
 
 String can use operators +, Connect two or more strings together.
@@ -14,31 +14,31 @@ When you have finished the work, click "Run Tests" to see if your code is workin
 misson 2: After misson 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3
 
 If everything is right, click "Submit" again to submit your code pass this kata.
-
+ */
 var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
 function Dad(){
   //select some variable to combine "Dad"
-  return ?+?+?;
+  return d1+a2+d2;
 }
 function Bee(){
   //select some variable to combine "Bee"
-  return ?+?+?;
+  return b1+e2+e2;
 }
 function banana(){
   //select some variable to combine "banana"
-  return ?+?+?;
+  return b2+a2+n2+a2+n2+a2;
 }
 
 //answer some questions if you finished works above
 function answer1(){
   //the answer should be "yes" or "no"
-  return "";
+  return "no";
 }
 function answer2(){
   //the answer should be "yes" or "no"
-  return "";
+  return "no";
 }
 function answer3(){
   //the answer should be "yes" or "no"
-  return "";
+  return "yes";
 }
