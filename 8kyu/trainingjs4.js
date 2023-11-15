c0660c086b382deddc6e8655737c6b9a8d87d4c2
pedram-pema <studentpedramp@gmail.com>
2023-11-15 17:59:49 +0330

    Complete the 4th training

--- 8kyu/trainingjs4.js
+++ 8kyu/trainingjs4.js
@@ -15,24 +15,24 @@ In the end, click "Submit" to submit your code pass this kata. */
 
 function getLength(arr){
     //return length of arr
-    return ?
+    return arr.length;
   }
   function getFirst(arr){
     //return the first element of arr
-    return ?
+    return arr[0];
   }
   function getLast(arr){
     //return the last element of arr
-    return ?
+    return arr[arr.length-1];
   }
   function pushElement(arr){
     var el=1;
     //push el to arr
-    
-    return arr
+    arr.push(el);
+    return arr;
   }
   function popElement(arr){
     //pop an element from arr
-    
-    return ?
+    arr.pop();
+    return arr;
   }
