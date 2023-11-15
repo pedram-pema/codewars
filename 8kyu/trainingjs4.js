8adc0cd8fa1da6907895b25abcec56400aa23bbf
pedram-pema <studentpedramp@gmail.com>
2023-11-15 17:31:30 +0330

    Setup the file of 4th training

---
+++ 8kyu/trainingjs4.js
@@ -0,0 +1,38 @@
+/* Training JS #4: Basic data types--Array
+https://www.codewars.com/kata/571effabb625ed9b0600107a/train/javascript */
+
+// TASK:
+/* I've written five functions. Each function receives a parameter arr which is an array. Complete the functions using arr inside the function bodies.
+
+    1. getLength(arr)    should return length of arr
+    2. getFirst(arr)     should return the first element of arr
+    3. getLast(arr)      should return the last element of arr
+    4. pushElement(arr)  should push an element to arr, and then return arr
+    5. popElement(arr)   should pop an element from arr, and then return arr
+When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+In the end, click "Submit" to submit your code pass this kata. */
+
+function getLength(arr){
+    //return length of arr
+    return ?
+  }
+  function getFirst(arr){
+    //return the first element of arr
+    return ?
+  }
+  function getLast(arr){
+    //return the last element of arr
+    return ?
+  }
+  function pushElement(arr){
+    var el=1;
+    //push el to arr
+    
+    return arr
+  }
+  function popElement(arr){
+    //pop an element from arr
+    
+    return ?
+  }
