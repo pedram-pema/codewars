ca711696e7acec9a5d50689bd9f2df3ea8e3d329
pedram-pema <studentpedramp@gmail.com>
2023-11-30 17:09:26 +0330

    move 8kyu directory into javascript

--- 8kyu/trainingjs1.js
+++
@@ -1,7 +0,0 @@
-/* Training JS #1: create your first JS function and print "Hello World!":
-https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8. */
-
-function helloWorld() {
-    let str = 'Hello World!';
-    console.log(str);
-}
