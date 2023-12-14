08b3eb483d5057aa57714c32c4e50096e088ecb1
pedram-pema <studentpedramp@gmail.com>
2023-12-14 17:12:17 +0330

    Complete simple_multiplication exercise

---
+++ javascript/8kyu/simple_multiplication.js
@@ -0,0 +1,19 @@
+/* 
+Simple multiplication
+https://www.codewars.com/kata/583710ccaa6717322c000105/train/javascript
+ */
+
+/* 
+Task:
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+ */
+
+function simpleMultiplication(number) {
+    function simpleMultiplication(number) {
+        if(number % 2 === 0) {
+          return number * 8;
+        } else {
+          return number * 9;
+        }
+      }      
+}
