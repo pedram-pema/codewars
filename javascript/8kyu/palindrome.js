b5fef2220a3dcadfcc1c57ab72c45772265175a9
pedram-pema <studentpedramp@gmail.com>
2024-01-07 09:47:03 +0330

    Setup and solve the palindrome exercise

---
+++ javascript/8kyu/palindrome.js
@@ -0,0 +1,21 @@
+/* 
+Is it a palindrome?
+https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
+ */
+
+/* 
+Task:
+Write a function that checks if a given string (case insensitive) is a palindrome.
+A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
+ */
+
+function isPalindrome(x) {
+    let caseInsensitive = x.toLowerCase();
+    let arrOfX = caseInsensitive.split('');
+    
+    let newArr = arrOfX.reverse();
+    
+    let reversed = newArr.join('');
+    
+    return reversed === caseInsensitive;
+}
