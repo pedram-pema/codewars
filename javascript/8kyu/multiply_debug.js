35a014b80da8e6c6cea79b258ed2843ea56a7229
pedram-pema <studentpedramp@gmail.com>
2024-01-14 07:12:31 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/multiply_debug.js
@@ -0,0 +1,12 @@
+/* 
+Multiply
+https://www.codewars.com/kata/50654ddff44f800200000004/train/javascript
+ */
+
+/* 
+This code does not execute properly. Try to figure out why.
+ */
+
+function multiply(a, b){
+    return a * b;
+}   
