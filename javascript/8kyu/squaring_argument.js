6c0e891197c6cbd8b624dc6e8d0dc54b095defba
pedram-pema <studentpedramp@gmail.com>
2024-01-15 11:42:21 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/squaring_argument.js
@@ -0,0 +1,12 @@
+/* 
+Function 2 - squaring an argument
+https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+ */
+
+/* 
+Now you have to write a function that takes an argument and returns the square of it.
+ */
+
+function square(arg) {
+    return Math.pow(arg, 2);
+}
