0752c91bb3b9d39f6dbca1f5ef4bc2bc347d3d60
pedram-pema <studentpedramp@gmail.com>
2024-01-21 07:25:15 +0330

    Setup and solve the exercise

--- javascript/8kyu/numberToString.js
+++ javascript/8kyu/numberToString.js
@@ -1,20 +1,17 @@
 /* 
-Convert a String to a Number!
-https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+Convert a Number to a String!
+https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
  */
 
 /* 
-Note: This kata is inspired by Convert a Number to a String!. Try that one too.
-Description
-We need a function that can transform a string into a number. What ways of achieving this do you know?
-Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
-Examples
-"1234" --> 1234
-"605"  --> 605
-"1405" --> 1405
-"-7" --> -7
+We need a function that can transform a number (integer) into a string.
+What ways of achieving this do you know?
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"
  */
 
-const stringToNumber = function(str){
-    return Number(str);
+function numberToString(num) {
+    return String(num);
 }
