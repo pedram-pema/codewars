7839caaaef9d33b51742d10c52e28216fa574611
pedram-pema <studentpedramp@gmail.com>
2024-01-27 07:07:21 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/opposite_number.js
@@ -0,0 +1,17 @@
+/* 
+Opposite number
+https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+ */
+
+/* 
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+Examples:
+
+1: -1
+14: -14
+-34: 34
+ */
+
+function opposite(number) {
+    return -number;
+}
