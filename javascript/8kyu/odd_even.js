00db51f2f9d662fb919338194f9874bc9ef4ac58
pedram-pema <studentpedramp@gmail.com>
2024-01-27 06:36:03 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/odd_even.js
@@ -0,0 +1,12 @@
+/* 
+Even or Odd
+https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+ */
+
+/* 
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ */
+
+function evenOrOdd(number) {
+    return (number % 2 === 0) ? "Even" : "Odd";
+}
