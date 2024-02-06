96cf8c32e153eb2df3a98c136c4ce28f4d3ec9a5
pedram-pema <studentpedramp@gmail.com>
2024-02-06 07:56:09 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/returning_strings.js
@@ -0,0 +1,12 @@
+/* 
+Returning Strings
+https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+ */
+
+/* 
+Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+ */
+
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+}
