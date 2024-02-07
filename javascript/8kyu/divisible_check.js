0b8b215676acc4a067a77792a52741ed39226e51
pedram-pema <studentpedramp@gmail.com>
2024-02-07 07:46:52 +0330

    Setup and solve the BMI exercise

--- javascript/8kyu/divisible_check.js
+++ javascript/8kyu/divisible_check.js
@@ -13,6 +13,11 @@ Examples:
  */
 
 function isDivisible(n, x, y) {
+    if (n % x === 0 && n % y === 0) {
+      return true;
+    } else {
+      return false;
+    }
     
     return ((n % x === 0) && (n % y === 0)) ? true : false;
     
