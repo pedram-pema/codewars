379c36f7053274e8e2c6f157ce7d74415defee0a
pedram-pema <studentpedramp@gmail.com>
2024-02-10 10:33:31 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/boolean_to_string.js
@@ -0,0 +1,12 @@
+/* 
+Convert a Boolean to a String
+https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/javascript
+ */
+
+/* 
+Implement a function which convert the given boolean value into its string representation.
+ */
+
+function booleanToString(b) {
+    return (b === true) ? "true" : "false";
+}
