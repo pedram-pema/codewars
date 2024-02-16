6cd37c1577fb7a97b2f32b817e695a512d7f4e15
pedram-pema <studentpedramp@gmail.com>
2024-02-16 18:15:32 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/grade_book.js
@@ -0,0 +1,32 @@
+/* 
+Grasshopper - Grade book
+https://www.codewars.com/kata/55cbd4ba903825f7970000f5/train/javascript
+ */
+
+/* 
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+ */
+
+function getGrade (s1, s2, s3) {
+  
+    let averageScore = (s1 + s2 + s3) / 3;
+    
+    if (averageScore >= 0 && averageScore < 60) {
+      return "F";
+    } else if (averageScore >= 60 && averageScore < 70) {
+      return "D";
+    } else if (averageScore >= 70 && averageScore < 80) {
+      return "C";
+    } else if (averageScore >= 80 && averageScore < 90) {
+      return "B";
+    } else {
+      return "A";
+    }
+}
