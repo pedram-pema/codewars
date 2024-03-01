9969982c69bae5c13664602fbc457530f8088cd1
pedram-pema <studentpedramp@gmail.com>
2024-03-01 14:29:05 +0330

    Create 7kyu directory and solve first exercise

---
+++ javascript/7kyu/square_test.js
@@ -0,0 +1,25 @@
+/* 
+You're a square!
+https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
+*/
+
+/* 
+Given an integral number, determine if it's a square number:
+In mathematics, a square number or perfect square is an integer that is the square of an integer;
+in other words, it is the product of some integer with itself.
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+Examples
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
+*/
+
+var isSquare = function(n){
+
+  let root = Math.floor(Math.sqrt(n));
+  return root*root === n;
+
+}
