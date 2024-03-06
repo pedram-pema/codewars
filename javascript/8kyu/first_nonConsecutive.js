5953dc7c6973756134e031560116eac38b8709d7
pedram-pema <studentpedramp@gmail.com>
2024-03-06 12:39:36 +0330

    Setup and solve the exercise

--- javascript/8kyu/first_nonConsecutive.js
+++ javascript/8kyu/first_nonConsecutive.js
@@ -1,6 +1,7 @@
-
+/* 
 Find the first non-consecutive number
 https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/javascript
+*/
 
 /* 
 Your task is to find the first element of an array that is not consecutive.
---
+++ javascript/8kyu/powers_of_two.js
@@ -0,0 +1,24 @@
+/* 
+Powers of 2
+https://www.codewars.com/kata/57a083a57cb1f31db7000028/train/javascript
+*/
+
+/* 
+Complete the function that takes a non-negative integer n as input.
+returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+*/
+
+function powersOfTwo(n) {
+    
+    let powers = [];
+    
+    for (let i = 0; i <= n; i++) {
+      powers.push(Math.pow(2, i));
+    }
+    
+    return powers;
+}
