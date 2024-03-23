57eae20d490326e23fe8660a438700feb7031574
pedram-pema <studentpedramp@gmail.com>
2024-03-23 05:57:18 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/invert_value.js
@@ -0,0 +1,16 @@
+/* 
+Invert values
+https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/train/javascript
+*/
+
+/* 
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.
+*/
+
+function invert(array) {
+    return array.map(int => int < 0 ? -int : -int);
+}
