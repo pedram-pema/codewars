e94bd820bf66025cc4827c7f1e6590e19af3f1f6
pedram-pema <studentpedramp@gmail.com>
2024-03-29 20:57:41 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/traffic_lights.js
@@ -0,0 +1,16 @@
+/* 
+Thinkful - Logic Drills: Traffic light
+https://www.codewars.com/kata/58649884a1659ed6cb000072/train/javascript
+*/
+
+/* 
+You're writing code to control your town's traffic lights.
+You need a function to handle each change from green, to yellow, to red, and then to green again.
+Complete the function that takes a string as an argument representing the current state of the light. 
+And returns a string representing the state the light should change to.
+For example, when the input is green, output should be yellow.
+*/
+
+function updateLight(current) {
+    return current === "green" ? "yellow" : (current === "yellow" ? "red" : "green");
+}
