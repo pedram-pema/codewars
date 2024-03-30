fbc16aa186e0f1b6209d9edac357b58c34f2d11d
pedram-pema <studentpedramp@gmail.com>
2024-03-30 14:37:53 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/pipe_problem.js
@@ -0,0 +1,42 @@
+/* 
+Lario and Muigi Pipe Problem
+https://www.codewars.com/kata/56b29582461215098d00000f/train/javascript
+*/
+
+/* 
+Given a list of unique numbers sorted in ascending order.
+Return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+Example
+Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+*/
+
+function pipeFix(numbers) {
+  
+    let min = Math.min(...numbers);
+    let max = Math.max(...numbers);
+    let sorted = [];
+    
+    for (let i = min; i <= max; i++) {
+      sorted.push(i);
+    }
+    
+    return sorted;
+}
+
+/* solve it using do while
+
+
+function pipeFix(numbers){
+  
+    let sorted = [];
+    
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] === numbers[i+1] + 1) {
+          sorted.push(numbers[i]);
+        } else if (numbers[i] !== numbers[i+1] + 1) {
+          sorted.push(numbers[i] + 1);
+        }
+      }
+    
+    return sorted;
+  } */
