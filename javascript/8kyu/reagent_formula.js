99cb89b9d66a1703af36b9c7bee93244a637813e
pedram-pema <studentpedramp@gmail.com>
2024-04-04 14:16:55 +0330

    Add a second solution with simple logicals

--- javascript/8kyu/reagent_formula.js
+++ javascript/8kyu/reagent_formula.js
@@ -44,3 +44,15 @@ function isValid(formula) {
   
     return rule1 && rule2 && rule3 && rule4;
 }
+
+// Add second solution:
+/* 
+function isValid(formula){
+  
+  return !(formula.includes(1) && formula.includes(2)) &&
+  !(formula.includes(3) && formula.includes(4)) &&
+    (formula.includes(5) === formula.includes(6)) &&
+    (formula.includes(7) || formula.includes(8));
+     
+}
+*/
