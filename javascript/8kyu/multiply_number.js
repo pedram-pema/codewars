eaeb8fd92ae45456a188901265425153beafa3c5
pedram-pema <studentpedramp@gmail.com>
2024-04-05 22:10:24 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/multiply_number.js
@@ -0,0 +1,21 @@
+/* 
+Multiply the number
+https://www.codewars.com/kata/5708f682c69b48047b000e07/train/javascript
+*/
+
+/* 
+Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers.
+so, for example:
+  3 -->    15  (  3 * 5¹)
+ 10 -->   250  ( 10 * 5²)
+200 --> 25000  (200 * 5³)
+  0 -->     0  (  0 * 5¹)
+ -3 -->   -15  ( -3 * 5¹)
+*/
+
+function multiply(number) {
+
+    let numLength = Math.abs(number).toString().split("").length;
+    return number * (Math.pow(5, numLength));
+    
+}
