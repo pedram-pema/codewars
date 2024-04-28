94048767a8dc8dd1c06268d51989380e6623f2cc
pedram-pema <studentpedramp@gmail.com>
2024-04-28 17:57:17 +0330

    Add a solution written with string replace method

--- javascript/8kyu/character_fixing.js
+++ javascript/8kyu/character_fixing.js
@@ -43,5 +43,11 @@ function correct(string)
     }
   }).join("");
 }
+*/
 
+/* 
+Add a solution written with string replace method:
+function correct(string) {
+  return string.replace(/0/g, 'O').replace(/1/g, 'I').replace(/5/g, 'S');
+}
 */
