b255607fa62cbe242b8e5c4aab9c6592ac654e44
pedram-pema <studentpedramp@gmail.com>
2024-05-06 19:22:04 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/square_root.js
@@ -0,0 +1,31 @@
+/* 
+To square(root) or not to square(root)
+https://www.codewars.com/kata/57f6ad55cca6e045d2000627/train/javascript
+*/
+
+/* 
+Write a method, that will get an integer array as parameter and will process every number from this array.
+Return a new array with processing every number of the input-array like this:
+If the number has an integer square root, take this, otherwise square the number.
+Example
+[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+Notes
+The input array will always contain only positive numbers, and will never be empty or null.
+*/
+
+function squareOrSquareRoot(array) {
+  
+    let expectedArr = [];
+    
+    for (let i = 0; i < array.length; i++) {
+      let root = Math.sqrt(array[i]);
+      if (Number.isInteger(root)) {
+        expectedArr.push(root);
+      } else {
+        expectedArr.push(Math.pow(array[i], 2));
+      }
+    }
+    
+    return expectedArr;
+    
+}
