1f7f82c881ed4ca464bc59759762b27a2c911fc8
pedram-pema <studentpedramp@gmail.com>
2024-05-11 13:30:21 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/stringy_strings.js
@@ -0,0 +1,30 @@
+/* 
+Stringy Strings
+https://www.codewars.com/kata/563b74ddd19a3ad462000054/train/javascript
+*/
+
+/* 
+write me a function stringy that takes a size and returns a string of alternating 1s and 0s.
+the string should start with a 1.
+a string with size 6 should return :'101010'.
+with size 4 should return : '1010'.
+with size 12 should return : '101010101010'.
+The size will always be positive and will only use whole numbers.
+*/
+
+function stringy(size) {
+  
+    let arr = [];
+    
+    for (let i = 0; i < size ; i++) {
+      if (i === 0) {
+        arr.push("1");
+      } else if (i % 2 === 0) {
+        arr.push("1");
+      } else if (i % 2 !== 0) {
+        arr.push("0");
+      }
+    }
+    
+    return arr.join("");
+}
