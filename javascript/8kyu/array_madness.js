2d312f68f930f847dcd0ade7c18109a08f3c05b2
pedram-pema <studentpedramp@gmail.com>
2024-05-31 15:51:20 +0330

    Setup and solve the execise

---
+++ javascript/8kyu/array_madness.js
@@ -0,0 +1,27 @@
+/* 
+SpeedCode #2 - Array Madness
+https://www.codewars.com/kata/56ff6a70e1a63ccdfa0001b1/train/javascript
+*/
+
+/* 
+Given two integer arrays a, b, both of length >= 1, 
+create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+E.g.
+arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+*/
+
+function arrayMadness(a, b) {
+  
+    let sumA = 0;
+    for (let i = 0; i < a.length; i++) {
+      sumA += Math.pow(a[i], 2);
+    }
+    
+    let sumB = 0;
+    for (let i = 0; i < b.length; i++) {
+      sumB += Math.pow(b[i], 3);
+    }
+    
+    return sumA > sumB;
+    
+}
