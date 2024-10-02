1e653605129da52342b017dd623d563d3c1de1f9
pedram-pema <studentpedramp@gmail.com>
2024-10-02 21:00:59 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/array_w_comma_delimiter.js
@@ -0,0 +1,17 @@
+/* 
+Printing Array elements with Comma delimiters
+https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+*/
+
+/* 
+Input: Array of elements
+["h","o","l","a"]
+Output: String with comma delimited elements of the array in th same order.
+"h,o,l,a"
+Note: if this seems too simple for you try the next level
+Note2: the input data can be: boolean array, array of objects, array of string arrays, array of number arrays... 😕
+*/
+
+function printArray(array){
+    return array.join(",");
+}
