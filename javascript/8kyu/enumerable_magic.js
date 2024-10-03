76046a8b7b9285e026d34bbc6e082fb146d74180
pedram-pema <studentpedramp@gmail.com>
2024-10-03 20:53:27 +0330

    Setup and solve the exercise

---
+++ javascript/8kyu/enumerable_magic.js
@@ -0,0 +1,12 @@
+/* 
+Enumerable Magic - Does My List Include This?
+https://www.codewars.com/kata/545991b4cbae2a5fda000158/train/javascript
+*/
+
+/* 
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+
+function include(arr, item){
+    return arr.includes(item);
+}
