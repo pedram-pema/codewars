e4dbbb4972ac62b0d79f7e1bf657f73c57b37c17
pedram-pema <studentpedramp@gmail.com>
2024-10-11 22:43:02 +0330

    Setup and solve the exercise

---
+++ javascript/7kyu/small_enough.js
@@ -0,0 +1,18 @@
+/* 
+Small enough? - Beginner
+https://www.codewars.com/kata/57cc981a58da9e302a000214/train/javascript
+*/
+
+/* 
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+You can assume all values in the array are numbers.
+*/
+
+function smallEnough(a, limit){
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] > limit) {
+        return false;   
+      }
+    }
+    return true;
+}
