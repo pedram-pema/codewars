ae1e13f9a688df8a69489615e4b1868ce07bbcff
pedram-pema <studentpedramp@gmail.com>
2024-11-14 18:06:27 +0330

    Setup and solve the exercise

---
+++ javascript/7kyu/is_it_letter.js
@@ -0,0 +1,12 @@
+/* 
+Is it a letter?
+https://www.codewars.com/kata/57a06b07cf1fa58b2b000252/train/javascript
+*/
+
+/* 
+Write a function, isItLetter or is_it_letter or IsItLetter, which tells us if a given character is a letter or not.
+*/
+
+function isItLetter(character) {
+    return /[a-z]/i.test(character);
+}
